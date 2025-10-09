2b4226da89b546466fdfc1ea5a19b0bfe5efeb31
douglasgvr <douglasfarmaangra@gmail.com>
2025-10-09 17:08:29 -0300

    Initial commit

---
+++ src/test/java/io/douglascoelho/CadastroDeNinja/CadastroDeNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.douglascoelho.CadastroDeNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
