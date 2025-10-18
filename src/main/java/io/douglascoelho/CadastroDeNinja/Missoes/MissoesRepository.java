df6948c4e3da879472352f8b42235a171629aa1a
douglasgvr <douglasfarmaangra@gmail.com>
2025-10-18 10:39:58 -0300

    feat: adiciona o JPA em Missoes

---
+++ src/main/java/io/douglascoelho/CadastroDeNinja/Missoes/MissoesRepository.java
@@ -0,0 +1,5 @@
+package io.douglascoelho.CadastroDeNinja.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {}
